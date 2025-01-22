187ec55136a8fdd6b85968e2877009f8df5500fe
Gurditt Khurana <gurditt.khurana@1mg.com>
2025-01-22 12:00:40 +0530

    feat: add harpoon

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -31,6 +31,7 @@ require('lazy').setup({
     require 'gurditt/plugins/treesitter',
 
     require 'gurditt/plugins/lazygit',
+    require 'gurditt/plugins/harpoon',
 }, {
     ui = {
         -- If you are using a Nerd Font: set icons to an empty table which will use the
